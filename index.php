cf4137d7e7dad08dabe057d19d800e0003ab38c9
Aimericv <aimericv17@gmail.com>
2023-05-02 09:51:42 +0200

    update

--- index.php
+++ index.php
@@ -1,20 +1,40 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Portfolio de [votre nom]</title>
+    <title>Portfolio de Vermesse Aimeric</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/css/bulma.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/js/bulma.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
+    <style>.hover:hover {
 
+  scale: 1.1;
+}
+
+.custom-card-height {
+  height: 520px;
+}
+
+
+
+</style>
   </head>
   <body>
     <section class="hero is-primary">
       <div class="hero-body">
         <div class="container">
           <h1 class="title">Portfolio de Aimeric Vermesse</h1>
+          <h4>
+            <a href="https://github.com/Aimericv" target="_blank" style="text-decoration: none;">
+              <i class="bi bi-github"></i> Github
+            </a>
+        
+            <a href="https://github.com/Aimericv" target="_blank" style="text-decoration: none;">
+              <i class="bi bi-github"></i> Linkedin
+            </a>
+        </h4>
           <nav class="navbar">
             <div class="navbar-menu">
               <div class="navbar-end">
@@ -84,8 +104,8 @@
           <div class="column">
             <div class="card">
               <div class="card-content">
-                <h3 class="title is-4">Stage 1</h3>
-                <p class="subtitle is-6">durée</p>
+                <h3 class="title is-4">Stage dans le service informatique de l'entreprise Duffau</h3>
+                <p class="subtitle is-6">23/05/2023 - 24/06/2023</p>
                 <ul>
                   <li></li>
                 </ul>
@@ -95,8 +115,8 @@
           <div class="column">
             <div class="card">
               <div class="card-content">
-                <h3 class="title is-4">Stage 2 </h3>
-                <p class="subtitle is-6">Durée:</p>
+                <h3 class="title is-4">Stage en tant que développeur web dans l'entreprise Soludevi</h3>
+                <p class="subtitle is-6">02/01/2023 - 15/02/2023</p>
                 <ul>
                   <li></li>
                 </ul>
@@ -111,14 +131,24 @@
     <section id="projects" class="section">
 
     <div class="modal fade" id="project1Modal" tabindex="-1" aria-labelledby="project1ModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
+  <div class="modal-dialog modal-dialog-centered modal-xl">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="project1ModalLabel">Nom du projet 1</h5>
+        <h1 class="modal-title" id="project1ModalLabel">Cybervote</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <p>Description du projet 1</p>
+        <h5>Dans le cadre de mon épreuve E5 j'ai réalisé un site de vote en ligne nommé CyberVote.</h5>
+        <p>Authentification par carte de vote</p>
+        <img src='carte_vote.PNG'></img>
+        <p>Sélection de l'éléction pour laquelle on veut voter</p>
+        <img src='election.PNG'></img>
+        <p>Vote pour le candidat de notre choix</p>
+        <img src='vote.PNG'></img>
+        <p>Authentification avec code secret, nom, prénom, code postal, date de naissance</p>
+        <img src='authentification.PNG'></img>
+        <p>Accès à la page des rédultats</p>
+        <img src='resultat.PNG'></img>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
@@ -128,14 +158,14 @@
 </div>
 
 <div class="modal fade" id="project2Modal" tabindex="-1" aria-labelledby="project2ModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
+  <div class="modal-dialog modal-dialog-centered modal-xl">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="project2ModalLabel">Nom du projet 2</h5>
+        <h1 class="modal-title" id="project2ModalLabel">Nom du projet 2</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <p>Description du projet 2</p>
+        <h5>Description du projet 2</h5>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
@@ -145,14 +175,55 @@
 </div>
 
 <div class="modal fade" id="project3Modal" tabindex="-1" aria-labelledby="project3ModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
+  <div class="modal-dialog modal-dialog-centered modal-xl">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="project3ModalLabel">Nom du projet 3</h5>
+        <h1 class="modal-title" id="project3ModalLabel">Script Excel</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <p>Description du projet 3</p>
+      <h5>Création d'un script VBA pour enlever les caractères spéciaux et 0 non significatifs pour faciliter le travail des employés</h5>
+      <img src='tab.PNG'></img>
+        <p></p>
+        <img src='vba.PNG'></img>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="project4Modal" tabindex="-1" aria-labelledby="project4ModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-xl">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title" id="project4ModalLabel">Modification mot de passe</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <h5>Modification des mots de passe des employés de l'entreprise Duffau pour respecter les normes de sécurité</h5>
+        <img src='mdp.PNG'></img>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="project5Modal" tabindex="-1" aria-labelledby="project5ModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-xl">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title" id="project5ModalLabel">dhtfh</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <h5>ygfjuyguty</h5>      
+
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
@@ -165,45 +236,102 @@
     <h2 class="title">Mes projets</h2>
     <div class="columns is-multiline">
       <div class="column is-one-third">
-        <div class="card" style="display:none;">
+      <div class="hover">
+        <div class="card" style="display:none; ">
+        <div class="card-content custom-card-height">
+
           <div class="card-image">
             <figure class="image is-4by3">
-            <img src="img.jpg" alt="[nom du projet]" data-bs-toggle="modal" data-bs-target="#project1Modal">            </figure>
+            <img src="Cybervote.jpg" alt="[nom du projet]" data-bs-toggle="modal" data-bs-target="#project1Modal">            </figure>
           </div>
           <div class="card-content">
-            <h3 class="title is-4">Nom du projet 1</h3>
-            <p>Description du projet 1</p>
+            <h3 class="title is-4">Cybervote</h3>
+            <p>Site permettant de voter à des éléctions en ligne</p>
+
 
           </div>
         </div>
+        </div>
+      </div>
       </div>
       <div class="column is-one-third">
+      <div class="hover">
+
         <div class="card" style="display:none;">
+        <div class="card-content custom-card-height">
+
           <div class="card-image">
             <figure class="image is-4by3">
             <img src="img.jpg" alt="[nom du projet]" data-bs-toggle="modal" data-bs-target="#project2Modal">            </figure>
           </div>
           <div class="card-content">
-            <h3 class="title is-4">Nom du projet 1</h3>
-            <p>Description du projet 1</p>
+            <h3 class="title is-4">Formulaire LeBonDebarasseur</h3>
+            <p>Formulaire facilitant l'insertion de nouvelles entreprises dans le site</p>
 
           </div>
         </div>
+        </div>
+      </div>
       </div>
       <div class="column is-one-third">
+      <div class="hover">
         <div class="card" style="display:none;">
+        <div class="card-content custom-card-height">
+
+
           <div class="card-image">
             <figure class="image is-4by3">
-            <img src="img.jpg" alt="[nom du projet]" data-bs-toggle="modal" data-bs-target="#project3Modal">            </figure>
+            <img src="excel.jpg" alt="[nom du projet]" data-bs-toggle="modal" data-bs-target="#project3Modal">            </figure>
           </div>
           <div class="card-content">
-            <h3 class="title is-4">Nom du projet 3</h3>
-            <p>Description du projet 3</p>
+            <h3 class="title is-4">Script Excel</h3>
+            <p>Création d'un script VBA pour enlever les caractères spéciaux et 0 non significatifs pour faciliter le travail des employés</p>
 
           </div>
+        </div>
       <!-- Ajoutez d'autres éléments de projet ici -->
     </div>
   </div>
+  
+</div>
+<div class="column is-one-third">
+      <div class="hover">
+        <div class="card" style="display:none;">
+        <div class="card-content custom-card-height">
+
+
+          <div class="card-image">
+            <figure class="image is-4by3">
+            <img src="securite-mdp.png" alt="[nom du projet]" data-bs-toggle="modal" data-bs-target="#project4Modal">            </figure>
+          </div>
+          <div class="card-content">
+            <h3 class="title is-4">Modification mots de passe</h3>
+            <p>Modification des mots de passes des employés de l'entreprise Duffau pour réspecter la norme de sécurité</p>
+
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+    <div class="column is-one-third">
+      <div class="hover">
+        <div class="card" style="display:none;">
+        <div class="card-content custom-card-height">
+
+
+          <div class="card-image">
+            <figure class="image is-4by3">
+            <img src="img.jpg" alt="[nom du projet]" data-bs-toggle="modal" data-bs-target="#project5Modal">            </figure>
+          </div>
+          <div class="card-content">
+            <h3 class="title is-4">Mise en place d'une veille technologique</h3>
+            <p>Mise en place d'une veille technologique afin d'être à jour concernant les nouvelles informations liées à l'informatque</p>
+            
+          </div>
+        </div>
+  </div>
+  
+</div> 
 </section>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
@@ -218,4 +346,7 @@ $(document).ready(function() {
 
 
 </script>
+<center><object data="tableau_synthese.pdf" type="application/pdf" width="50%" height="1000">
+  <p>Alternative text - include a link <a href="tableau_synthese.pdf">to the PDF!</a></p>
+</object></center>
 </section>
